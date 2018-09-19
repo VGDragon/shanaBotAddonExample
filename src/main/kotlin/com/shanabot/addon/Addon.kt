b32160ac26053b8cf59687a1929d999b9795e113
Patrick Promitzer <VG_Dragon@hotmail.com>
2018-09-19 02:26:25 +0200

    added documentation

--- src/main/kotlin/com/shanabot/addon/Addon.kt
+++ src/main/kotlin/com/shanabot/addon/Addon.kt
@@ -7,20 +7,28 @@ import com.vgdragon.discordbotaddon.DiscordAPI
 
 
 class Addon {
-    /*
-   * messageSplit = messageReceive.content.split(" ")
-   * it give you a slitted message to work with. You can split it yourself, too.
-   *
-   * startMessageSplitInt
-   * The bot normally use "shana <commandName> ..." and startMessageSplitInt would be 2.
-   * I could happen that the bot change that, for example to "s!<command> ...> and then startMessageSplitInt would be 1.
-   * It is better to use startMessageSplitInt to be save.
-   *
-    */
+
+
+    // multi Thread save
     val saveClassLock = Object()
+    // Class with variables to save with save()
     var saveClass = SaveClass()
 
 
+    /**
+     * Will be executed if someone write a text with the command name of the addon.
+     * Example: shana say
+     *
+     * @param discordAPI the discordAPI to use with the Bot.
+     * @param messageReceive the message Object from Discord
+     * @param messageSplit the message slitted with split(" ")
+     * @param startMessageSplitInt the first position in the messageSplit List you can use for your addon.
+     *        Example: Shana say hello
+     *                   0    1    2
+     *        startMessageSplitInt = 2
+     * @param botCommandStartString the text to invoke the bot (currently "shana ")
+     *
+     */
     public fun onMessageCreate(discordAPI: DiscordAPI, messageReceive: MessageReceive, messageSplit: List<String>, startMessageSplitInt: Int, botCommandStartString: String) {
         synchronized(saveClassLock){
             saveClass.counter++
@@ -33,12 +41,32 @@ class Addon {
         discordAPI.sendMessage(string.trim(), messageReceive.channel_id)
     }
 
+    /**
+     * The Bot remembers the messageID of the message your Addon was sending, for a short amount of time (currently 5min).
+     * If someone add A Reaction of your Addons message, this function will be called.
+     *
+     * @param discordAPI the discordAPI to use with the Bot.
+     * @param messageReactionObject the Reaction Object from Discord.
+     */
     public fun onMessageReactionAdd(discordAPI: DiscordAPI, messageReactionObject: MessageReactionObject){
         if(!messageReactionObject.emoji.name.equals("❌"))
             return
         discordAPI.deleteMessage(messageReactionObject.message_id)
     }
 
+    /**
+     * If someone need help with your Addon, he/she/apache_helicopter can use the command "help $commandName".
+     * This function will be called if someone need help with your Addon.
+     *
+     * @param messageSplit the message slitted with split(" ")
+     * @param startMessageSplitInt the first position in the messageSplit List you can use for your addon.
+     *             Example: Shana say hello
+     *                        0    1    2
+     *             startMessageSplitInt = 2
+     * @param botCommandStartString the text to invoke the bot (currently "shana ")
+     *
+     * @return The Embed Object that will be added to the answer message.
+     */
     public fun help(messageSplit: List<String>, startMessageSplitInt: Int, botCommandStartString: String): Embed {
         val embed = Embed()
         embed.title = "Command: say"
@@ -50,13 +78,27 @@ class Addon {
         return embed
     }
 
+    /**
+     * In this you can write something about your Addon.
+     * This will be displayed only on the Website and should be a short Text.
+     * Everyone should understand the purpose of this Addon is and what it does.
+     *
+     * @param botCommandStartString the text to invoke the bot (currently "shana ")
+     * @return text that is displayed on the Website.
+     */
     public fun description(botCommandStartString: String): String {
-
         return "The command let shana write the text after the command name.\n" +
                 "Example: ${botCommandStartString}say Hi, I am Shana.\n" +
                 "Shana: Hi, I am Shana.\n"
     }
 
+    /**
+     * This function will be executed over time (currently every 5min), if the Bot Admin remove your Addon from the Bot
+     * or the bot will be closed from the Admin(Bot update).
+     * You can use this function to saving important variables like rights, counters, ...
+     *
+     * @return String to save (normally a json)
+     */
     public fun save(): String{
         var saveString = ""
         synchronized(saveClassLock){
@@ -69,6 +111,12 @@ class Addon {
         }
         return saveString
     }
+    /**
+     * This function will be executed if this Addon will be added to the Bot from an Admin or the Bot start.
+     * You can use this function to load saved variables
+     *
+     * @param string from the saved File (normally the String from save())
+     */
     public fun load(string: String) {
         synchronized(saveClassLock) {
             try {
@@ -78,8 +126,25 @@ class Addon {
             }
         }
     }
+
+    /**
+     * This is for sorting all active Addons on the Discord Server, in the help message.
+     * Currently is only one Type supported.
+     *
+     * @return String of the Name to sort (Example: Fun)
+     *
+     */
     public fun commandType(): String{
         return "Other"
+    }
+
+    /**
+     * This will be Executed if the Admin remove this Addon from the Bot or the Bot will be closed.
+     * If your Addon have to do something before, you can put it in this function.
+     * The save() function will be executed after the onExit() function.
+     */
+    public fun onExit(){
+        // nothing to do
     }
 }
 
@@ -90,4 +155,5 @@ help(messageSplit: List<String>, startMessageSplitInt: Int, botCommandStartStrin
 description(): String
 save(): String
 load(string: String)
+onExit()
  */
--- src/main/kotlin/com/shanabot/addon/SaveClass.kt
+++ src/main/kotlin/com/shanabot/addon/SaveClass.kt
@@ -8,9 +8,11 @@ import com.google.gson.JsonParser
 class SaveClass{
     var counter = 0
 
+    // convert this class (with variables) to a json
     fun save(): String{
         return Gson().toJson(this)
     }
+    // convert a Json to this class (with variables)
     fun load(string: String){
         val saveClass: SaveClass = Gson().fromJson(JsonParser().parse(string))
         counter = saveClass.counter
